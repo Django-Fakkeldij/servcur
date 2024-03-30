2a628de244f25e94c498393af35d5c25c6df749d
MISTER-Unchained <djangofakkeldij@gmail.com>
2024-03-30 22:22:01 +0100

    Log streams API implemented

--- src/api/projects/executor.rs
+++ src/api/projects/executor.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::BTreeMap,
     path::{Path, PathBuf},
     process::{Output, Stdio},
     sync::Arc,
@@ -12,11 +13,11 @@ use async_recursion::async_recursion;
 use tokio::{
     io::{AsyncBufReadExt, BufReader},
     process::Command,
-    sync::{broadcast, mpsc, RwLock},
+    sync::{broadcast, mpsc, Mutex, RwLock},
     task::JoinHandle,
     time::Instant,
 };
-use tracing::{debug, info, info_span, instrument, warn, Instrument};
+use tracing::{debug, info, info_span, instrument, trace, warn, Instrument};
 
 use crate::{
     config::BUILD_LOG_FOLDER,
@@ -95,6 +96,12 @@ impl ProjectIoHandle {
     }
 }
 
+#[derive(Debug, Clone)]
+struct OutputSendHandle {
+    stdout: broadcast::Sender<String>,
+    stderr: broadcast::Sender<String>,
+}
+
 #[derive(Debug)]
 pub struct OutputHandle {
     stdout: broadcast::Receiver<String>,
@@ -103,13 +110,15 @@ pub struct OutputHandle {
 
 impl OutputHandle {
     /// returns stdout, stderr, self
-    pub fn new() -> (broadcast::Sender<String>, broadcast::Sender<String>, Self) {
+    fn new() -> (OutputSendHandle, Self) {
         let (stdout_sender, stdout_recv) = broadcast::channel(128);
         let (stderr_sender, stderr_recv) = broadcast::channel(128);
 
         (
-            stdout_sender,
-            stderr_sender,
+            OutputSendHandle {
+                stdout: stdout_sender,
+                stderr: stderr_sender,
+            },
             Self {
                 stdout: stdout_recv,
                 stderr: stderr_recv,
@@ -129,32 +138,36 @@ impl Clone for OutputHandle {
 
 #[derive(Debug)]
 pub struct ProjectIoExecutor {
-    exec_tx: Arc<mpsc::Sender<ProjectIoHandle>>,
-    output_handles: Arc<RwLock<OutputHandle>>,
+    build_id_counter: Arc<Mutex<usize>>,
+    exec_tx: Arc<mpsc::Sender<(usize, ProjectIoHandle)>>,
+    output_handles: Arc<RwLock<BTreeMap<usize, OutputHandle>>>,
     _exec_handle: JoinHandle<()>,
 }
 
 impl ProjectIoExecutor {
     pub fn new(size: usize) -> Self {
-        let (tx, mut rx) = mpsc::channel::<ProjectIoHandle>(size);
+        let (tx, mut rx) = mpsc::channel::<(usize, ProjectIoHandle)>(size);
 
+        let output_handles = Arc::new(RwLock::new(BTreeMap::new()));
+
+        let output_handles_clone = output_handles.clone();
         let _exec_handle = tokio::spawn(
             async move {
                 debug!("started executor");
                 loop {
-                    let handle = if let Some(v) = rx.recv().await {
+                    let (id, handle) = if let Some((i, v)) = rx.recv().await {
                         debug!(?v.project, "received io_handle");
-                        v
+                        (i, v)
                     } else {
                         warn!("emtpy recv");
                         continue;
                     };
+                    let (output_sender, output) = OutputHandle::new();
+                    let output_handles_clone_clone = output_handles_clone.clone();
                     tokio::spawn(async move {
-                        info!("started executing");
-                        let t = Instant::now();
-                        let _ = traced_execute_handle(handle).await;
-                        let duration = t.elapsed().as_millis();
-                        info!(duration_milisecs = duration, "finished executing")
+                        output_handles_clone_clone.write().await.insert(id, output);
+                        let _ = execute_handle_manager(handle, output_sender).await;
+                        output_handles_clone_clone.write().await.remove(&id);
                     });
                 }
             }
@@ -162,20 +175,44 @@ impl ProjectIoExecutor {
         );
 
         Self {
+            build_id_counter: Arc::new(Mutex::new(0)),
             _exec_handle,
             exec_tx: Arc::new(tx),
+            output_handles,
         }
     }
 
-    pub async fn exec(&self, handle: ProjectIoHandle) -> Result<()> {
-        self.exec_tx.send(handle).await.map_err(|e| e.into())
+    pub async fn exec(&self, handle: ProjectIoHandle) -> Result<usize> {
+        let id = {
+            let mut id = self.build_id_counter.lock().await;
+            *id += 1;
+            *id
+        };
+        self.exec_tx
+            .send((id, handle))
+            .await
+            .context("could not send handle to executor")?;
+        Ok(id)
+    }
+
+    pub async fn get_handles(
+        &self,
+    ) -> tokio::sync::RwLockReadGuard<'_, BTreeMap<usize, OutputHandle>> {
+        self.output_handles.read().await
+    }
+
+    pub async fn get_handle_by_id(&self, id: usize) -> Option<OutputHandle> {
+        self.output_handles.read().await.get(&id).cloned()
     }
 }
 
 #[async_recursion]
-async fn execute_handle(mut handle: ProjectIoHandle) -> Result<()> {
+async fn execute_handle(
+    mut handle: ProjectIoHandle,
+    output_handle: OutputSendHandle,
+) -> Result<()> {
     if let Some(child) = handle.depends_on {
-        execute_handle(*child).await?;
+        execute_handle(*child, output_handle.clone()).await?;
     }
 
     let mut command_handle = handle
@@ -192,7 +229,9 @@ async fn execute_handle(mut handle: ProjectIoHandle) -> Result<()> {
         _guard1 = tokio::spawn(async move {
             let mut lines = reader.lines();
             while let Ok(Some(l)) = lines.next_line().await {
-                println!("stdout reader: {}", l);
+                if let Err(e) = output_handle.stdout.send(l) {
+                    trace!(?e, "stdout sending error");
+                };
             }
         });
     }
@@ -203,7 +242,9 @@ async fn execute_handle(mut handle: ProjectIoHandle) -> Result<()> {
         _guard2 = tokio::spawn(async move {
             let mut lines = reader.lines();
             while let Ok(Some(l)) = lines.next_line().await {
-                println!("stderr reader: {}", l);
+                if let Err(e) = output_handle.stderr.send(l) {
+                    trace!(?e, "stdout sending error");
+                };
             }
         });
     }
@@ -233,10 +274,13 @@ async fn execute_handle(mut handle: ProjectIoHandle) -> Result<()> {
 
 #[instrument(err, name = "IoHandleExecute", level = "info")]
 #[allow(clippy::blocks_in_conditions)]
-async fn traced_execute_handle(handle: ProjectIoHandle) -> Result<()> {
+async fn execute_handle_manager(
+    handle: ProjectIoHandle,
+    output_handle: OutputSendHandle,
+) -> Result<()> {
     let t0 = Instant::now();
     info!("started IoHandle");
-    let ret = execute_handle(handle).await;
+    let ret = execute_handle(handle, output_handle).await;
     let d = t0.elapsed();
     info!(duration=?d, "finished IoHandle");
     ret
