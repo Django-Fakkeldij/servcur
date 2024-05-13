ba241c6a3980cdb30ef498da83916ec9d82f77df
MISTER-Unchained <djangofakkeldij@gmail.com>
2024-05-13 12:58:00 +0200

    Fixed container streaming

--- public/servcur/src/routes/containers/[slug]/+page.svelte
+++ public/servcur/src/routes/containers/[slug]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import ConsoleViewer from '$lib/ConsoleViewer.svelte';
 	import { API_ROUTES } from '$lib/api.js';
+	import type { ConsoleMessage } from '$lib/models/console.js';
+	import { dateString } from '$lib/util.js';
 	import { Card, Heading } from 'flowbite-svelte';
 	import { onDestroy } from 'svelte';
 
@@ -9,8 +11,10 @@
 	const ws = new WebSocket(API_ROUTES.containers_logs_ws(data.slug, Math.round(Date.now() / 1000) - 200));
 	const channel = new MessageChannel();
 	channel.port1.start();
+	channel.port1.postMessage({ line: `Connected to container logs`, class: 'text-gray-600 opacity-50' } satisfies ConsoleMessage);
 	ws.onmessage = (ev) => {
-		channel.port1.postMessage(ev.data);
+		const t = dateString(new Date());
+		channel.port1.postMessage({ line: t + ': ' + ev.data } satisfies ConsoleMessage);
 	};
 
 	onDestroy(() => {
